bdf3e8365f4dced64c10a7cbad6843e50cc14848
caioesr <caioesr@al.insper.edu.br>
2020-10-03 18:21:24 -0300

    and consertado

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/XorGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/XorGate.java
@@ -1,30 +1,33 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
-public class OrGate extends Gate {
+public class XorGate extends Gate {
     private final NandGate[] nands;
 
-    public OrGate()
-    {
-        super("OR", 2);
-        nands = new NandGate[3];
+    public XorGate() {
+        super("XOR", 2);
+
+        nands = new NandGate[4];
     }
 
     @Override
     public boolean read() {
-        return nands[2].read();
+        return nands[3].read();
     }
 
     @Override
     public void connect(int inputIndex, Emitter emitter) {
+        System.out.println(nands[0]);
         if (inputIndex < 0 || inputIndex > 1) {
             throw new IndexOutOfBoundsException(inputIndex);
         }
         nands[0].connect(0, emitter);
         nands[0].connect(1, emitter);
         nands[1].connect(0, emitter);
-        nands[1].connect(1, emitter);
-
+        nands[1].connect(1, nands[0]);
         nands[2].connect(0, nands[0]);
-        nands[2].connect(1, nands[1]);
+        nands[2].connect(1, emitter);
+        nands[3].connect(0, nands[1]);
+        nands[3].connect(1, nands[2]);
     }
+
 }
